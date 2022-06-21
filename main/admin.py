5b6ad68b4b1b6ac7e80eeca2f0f0932708426285
anara01 <anaraerkebaeva2@gmail.com>
2022-06-21 20:05:40 +0300

    create bottleShop project & main app & write models

---
+++ main/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Size)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'brand', 'color', 'size', 'status')
+    list_editable = ('status',)
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+# Product Attribute
+class ProductAttributeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'price', 'color', 'size')
+
+
+admin.site.register(ProductAttribute, ProductAttributeAdmin)
