4f33ff07d08c16e5f338a9b07ce6d521c691f503
anara01 <anaraerkebaeva2@gmail.com>
2022-06-22 11:30:15 +0300

    created category page and media root

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from .views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', home, name='home'),
+    path('category-list', category_list, name='category-list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from .models import Category
 
 # Home Page
+
+
 def home(request):
     return render(request, 'index.html')
+
+
+# Category
+def category_list(request):
+    data = Category.objects.all().order_by('-id')
+    return render(request, 'category_list.html', {'data': data})
+
+
+# Brands
